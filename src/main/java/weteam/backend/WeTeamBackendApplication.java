769f9d59090a616ddd7172c00a079bd7424006b0
klaus9267 <klaus9267@gmail.com>
2023-11-07 15:10:49 +0900

    Feat: createAT, updateAt을 위한 auditing 설정

--- src/main/java/weteam/backend/WeTeamBackendApplication.java
+++ src/main/java/weteam/backend/WeTeamBackendApplication.java
@@ -2,8 +2,10 @@ package weteam.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WeTeamBackendApplication {
 
     public static void main(String[] args) {
