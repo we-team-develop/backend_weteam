9ded318b0d1faee8b05c63a842daebead40cc949
klaus9267 <klaus9267@gmail.com>
2023-11-09 16:42:43 +0900

    Chore: etc

--- src/main/java/weteam/backend/WeTeamBackendApplication.java
+++ src/main/java/weteam/backend/WeTeamBackendApplication.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class WeTeamBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WeTeamBackendApplication.class, args);
     }
