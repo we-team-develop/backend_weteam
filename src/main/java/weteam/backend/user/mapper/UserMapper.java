ace346170a9347d42fd14d4993ce304101c992dc
klaus9267 <klaus9267@gmail.com>
2023-11-09 16:41:59 +0900

    Feat: joingRequest -> entity mapping code 작성

--- src/main/java/weteam/backend/user/mapper/UserMapper.java
+++ src/main/java/weteam/backend/user/mapper/UserMapper.java
@@ -1,16 +1,22 @@
 package weteam.backend.user.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 import weteam.backend.user.domain.User;
+import weteam.backend.user.domain.dto.JoinRequest;
 import weteam.backend.user.domain.dto.UserResponse;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING,
         unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
     UserMapper instance = Mappers.getMapper(UserMapper.class);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "image", ignore = true)
+    User toEntity(JoinRequest request);
 
     UserResponse toRes(User user);
 }
