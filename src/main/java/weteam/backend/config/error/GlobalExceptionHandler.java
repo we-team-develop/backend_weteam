420cec4398a9d77ddc8c95877456d626f546ccd1
klaus9267 <klaus9267@gmail.com>
2023-11-21 19:35:47 +0900

    Refactor: argument error 변경

--- src/main/java/weteam/backend/config/error/GlobalExceptionHandler.java
+++ src/main/java/weteam/backend/config/error/GlobalExceptionHandler.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import weteam.backend.config.dto.ErrorResponse;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 @Slf4j
@@ -23,11 +25,18 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        ObjectError error = e.getBindingResult().getAllErrors().get(0);
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.builder()
-                                                                               .field(Objects.requireNonNull(error.getCodes())[1])
-                                                                               .defaultMessage(error.getDefaultMessage())
-                                                                               .build());
+        List< ObjectError> errorList = e.getBindingResult().getAllErrors();
+        List<ErrorResponse> errorResponseList = errorList.stream().map(error->{
+            return ErrorResponse.builder()
+                                .field(error.getCode())
+                                .defaultMessage(error.getDefaultMessage())
+                                .build();
+        }).toList();
+
+//        log.error(errorList.toString());
+        errorList.forEach(error -> log.error(error.toString()));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponseList);
     }
 
 }
