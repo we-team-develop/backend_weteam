8e725a4c25068a562b7b7216449daff2cafc2a91
klaus9267 <klaus9267@gmail.com>
2023-11-23 15:44:21 +0900

    Chore: 초기값 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,6 @@
 #         ('# ESTP', 2),('# ESTJ', 2),('# ESFJ', 2),('# ESFP', 2),
 #         ('# INFP', 2),('# INFJ', 2),('# INTP', 2),('# INFJ', 2),
 #        ('# ISFP', 2),('# ISFJ', 2),('# ISTP', 2),('# ISFJ', 2);
+# insert into member(uid, username, password, nickname)
+# values ('admin', '김성현', '{bcrypt}$2a$10$rZh6102niouMyIXl2cl7k.GMPkP9G/r9oJzsaqzCr6296aNrMhD.G', 'admin'),
+#        ('test', 'test', '{bcrypt}$2a$10$Sa/v2tAJClb78tC5IXVkbeTYvMVmqcjFO0SX3Jds6QPaI.nYkQ5Mu', 'test');
