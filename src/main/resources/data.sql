9c737abb6d9bdffc2d98689a5c560d0e8b44528f
klaus9267 <klaus9267@gmail.com>
2023-11-24 20:50:51 +0900

    Chore: 기초 데이터 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,6 +3,13 @@
 #         ('# ESTP', 2),('# ESTJ', 2),('# ESFJ', 2),('# ESFP', 2),
 #         ('# INFP', 2),('# INFJ', 2),('# INTP', 2),('# INFJ', 2),
 #        ('# ISFP', 2),('# ISFJ', 2),('# ISTP', 2),('# ISFJ', 2);
-# insert into member(uid, username, password, nickname)
-# values ('admin', '김성현', '{bcrypt}$2a$10$rZh6102niouMyIXl2cl7k.GMPkP9G/r9oJzsaqzCr6296aNrMhD.G', 'admin'),
-#        ('test', 'test', '{bcrypt}$2a$10$Sa/v2tAJClb78tC5IXVkbeTYvMVmqcjFO0SX3Jds6QPaI.nYkQ5Mu', 'test');
+
+# insert into member(nickname, username)
+# values ('admin', 'admin'),
+#        ('test', '김민호');
+
+# insert into auth(uid, password)
+# values ('admin', '{bcrypt}$2a$10$rZh6102niouMyIXl2cl7k.GMPkP9G/r9oJzsaqzCr6296aNrMhD.G'),
+#        ('test', '{bcrypt}$2a$10$Sa/v2tAJClb78tC5IXVkbeTYvMVmqcjFO0SX3Jds6QPaI.nYkQ5Mu');
+
+
